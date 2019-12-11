622fe7bb46f3885ce6c930ce400f5bd09864aac7
Mike M <mikemiles19@gmail.com>
2019-12-11 12:05:18 -0700

    removed unnecessary stuff, also need to add test files and fix CMakeLists

--- include/ros_queue_to_disk.hpp
+++ include/ros_queue_to_disk.hpp
@@ -10,19 +10,19 @@
 
 namespace fs = std::filesystem;
 
-void demo_perms(fs::perms p)
-{
-    std::cout << ((p & fs::perms::owner_read) != fs::perms::none ? "r" : "-")
-              << ((p & fs::perms::owner_write) != fs::perms::none ? "w" : "-")
-              << ((p & fs::perms::owner_exec) != fs::perms::none ? "x" : "-")
-              << ((p & fs::perms::group_read) != fs::perms::none ? "r" : "-")
-              << ((p & fs::perms::group_write) != fs::perms::none ? "w" : "-")
-              << ((p & fs::perms::group_exec) != fs::perms::none ? "x" : "-")
-              << ((p & fs::perms::others_read) != fs::perms::none ? "r" : "-")
-              << ((p & fs::perms::others_write) != fs::perms::none ? "w" : "-")
-              << ((p & fs::perms::others_exec) != fs::perms::none ? "x" : "-")
-              << '\n';
-}
+// void demo_perms(fs::perms p)
+// {
+//     std::cout << ((p & fs::perms::owner_read) != fs::perms::none ? "r" : "-")
+//               << ((p & fs::perms::owner_write) != fs::perms::none ? "w" : "-")
+//               << ((p & fs::perms::owner_exec) != fs::perms::none ? "x" : "-")
+//               << ((p & fs::perms::group_read) != fs::perms::none ? "r" : "-")
+//               << ((p & fs::perms::group_write) != fs::perms::none ? "w" : "-")
+//               << ((p & fs::perms::group_exec) != fs::perms::none ? "x" : "-")
+//               << ((p & fs::perms::others_read) != fs::perms::none ? "r" : "-")
+//               << ((p & fs::perms::others_write) != fs::perms::none ? "w" : "-")
+//               << ((p & fs::perms::others_exec) != fs::perms::none ? "x" : "-")
+//               << '\n';
+// }
 
 namespace ros_queue_to_disk {
 // using namespace fs = std::filesystem;
